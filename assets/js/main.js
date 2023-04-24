738ca5dcb6e6f31e4a620dfefb3c47e969a73f94
Marcos <marcoswodbarros@outlook.com>
2023-04-24 16:21:38 -0300

    Refactoring js

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,29 +7,46 @@ let mainButton = document.querySelector('.main__button--container');
 let mainTexts = document.querySelector('.main__texts--container');
 let mainImages = document.querySelector('.main__images--container');
 
+let laptop = window.matchMedia("(min-width:1024px)");
+
 
 // Events
 openMenu.addEventListener("click", menuOpened);
 closeMenu.addEventListener("click", menuClosed);
 
+laptop.addEventListener('change', noHidden);
 
 // Functions
-function menuOpened () {
+function menuOpened() {
     openMenu.style.display = "none";
     closeMenu.style.display = "block";
     nav.style.display = "block";
-    h1.style.display = "none"; 
+    h1.style.display = "none";
     mainButton.style.display = "none";
-    mainTexts.style.display = "none"; 
-    mainImages.style.display = "none"; 
+    mainTexts.style.display = "none";
+    mainImages.style.display = "none";
 }
 
-function menuClosed () {
+function menuClosed() {
     openMenu.style.display = "block";
     closeMenu.style.display = "none";
     nav.style.display = "none";
     h1.style.display = "inline";
     mainButton.style.display = "flex";
-    mainTexts.style.display = "block"; 
+    mainTexts.style.display = "block";
     mainImages.style.display = "flex";
+}
+
+function noHidden() {
+    if (window.matchMedia("(min-width:1024px)").matches) {
+        nav.style.display = "block";
+        h1.style.display = "none";
+        mainButton.style.display = "none";
+    }
+
+    else if (window.matchMedia("(min-width:375px)").matches) {
+        nav.style.display = "none";
+        h1.style.display = "block";
+        mainButton.style.display = "flex";
+    }
 }
